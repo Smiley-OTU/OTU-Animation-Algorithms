bd5b64e90b1e0c8a40e12154c27820174f14a776
Smiley98 <smileysofficialcinema@gmail.com>
2022-09-16 18:54:21 -0400

    Rotating using lerp instead of cos

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,20 +6,35 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] GameObject target;
-    [SerializeField] float fov = 60.0f;
-    [SerializeField] float length = 10.0f;
+    [SerializeField] const float fov = 60.0f;
+    [SerializeField] const float length = 10.0f;
+    [SerializeField] const float rotationAmount = 90.0f;
+    [SerializeField] const float rotationDuration = 3.0f;
 
-    private float initialRotation;
+    private float rotSrc, rotDst;
+    private float elapsedTime = 0.0f;
+    private bool right = true;
 
     void Start()
     {
-        initialRotation = transform.rotation.eulerAngles.y;
+        rotSrc = transform.rotation.eulerAngles.y;
+        rotDst = rotSrc + rotationAmount;
     }
 
     void Update()
     {
+        float y = right ? Mathf.Lerp(rotSrc, rotDst, elapsedTime / rotationDuration) :
+            Mathf.Lerp(rotDst, rotSrc, elapsedTime / rotationDuration);
+        elapsedTime += Time.smoothDeltaTime;
+        if (elapsedTime > rotationDuration)
+        {
+            elapsedTime = 0.0f;
+            right = !right;
+        }
+
         Vector3 euler = transform.eulerAngles;
-        euler.y = initialRotation + Mathf.Cos(Time.realtimeSinceStartup) * Mathf.Rad2Deg;
+        euler.y = y;
+        //euler.y = rotSrc + Mathf.Cos(Time.realtimeSinceStartup) * Mathf.Rad2Deg;
         transform.eulerAngles = euler;
         Utility.InRangeDebug(transform, target.transform, length, fov);
     }
