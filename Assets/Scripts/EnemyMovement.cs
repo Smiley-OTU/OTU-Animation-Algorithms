76b0e9e3b19b696f22d52111747a8011aca4ab09
Smiley98 <smileysofficialcinema@gmail.com>
2022-09-16 16:37:53 -0400

    Completed fov check

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -17,7 +17,7 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0.0f, Mathf.Cos(Time.realtimeSinceStartup), 0.0f));
+        //transform.Rotate(new Vector3(0.0f, Mathf.Cos(Time.realtimeSinceStartup), 0.0f));
         Vector3 front = transform.rotation.eulerAngles;
         Vector3 left = front;
         Vector3 right = front;
@@ -29,11 +29,21 @@ public class EnemyMovement : MonoBehaviour
         ql.eulerAngles = left;
         qr.eulerAngles = right;
 
+        // We want to determine whether the angle between the player and enemy is greater than half the enemy's fov
+        // We can solve for this angle using the dot product
+        // a . b = ||a|| * ||b|| * cos(x)
+        // We can simplify this equation because we know that a and b are normalized, so we can remove their magnitude
+        // a . b = ||a|| * ||b|| * cos(x) = a . b = 1 * 1 * cos(x) = a . b = cos(x)
+        // Furthermore, we can compare a . b to cos(x) instead of solving for x to reduce complexity.
+        // result = a . b <= cos(x / 2) ? seen : unseen
+
+        Vector3 playerDirection = (target.transform.position - transform.position).normalized;
+        Vector3 enemyDirection = transform.rotation.normalized * Vector3.forward;
+        Color color = Vector3.Dot(playerDirection, enemyDirection) <= Mathf.Cos(Mathf.Deg2Rad * (fov * 0.5f))
+            ? Color.green : Color.red;
+
         // Draw lines green if unseen and red if seen
-        Debug.DrawLine(transform.position, transform.position + ql * Vector3.forward * length);
-        Debug.DrawLine(transform.position, transform.position + qr * Vector3.forward * length);
-        Debug.DrawLine(transform.position, target.transform.position);
-
-
+        Debug.DrawLine(transform.position, transform.position + ql * Vector3.forward * length, color);
+        Debug.DrawLine(transform.position, transform.position + qr * Vector3.forward * length, color);
     }
 }
