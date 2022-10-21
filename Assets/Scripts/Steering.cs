d364ffaa54f81b01bd62f86cf242932f8ea732e6
Smiley98 <smileysofficialcinema@gmail.com>
2022-10-21 16:18:24 -0400

    Added constant seeking and linear seeking

--- Assets/Scripts/Steering.cs
+++ Assets/Scripts/Steering.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Steering : MonoBehaviour
 {
@@ -14,7 +15,16 @@ public class Steering : MonoBehaviour
 
     private Rigidbody rb;
 
-    // Start is called before the first frame update
+    public enum SteeringBehaviour
+    {
+        SEEK_CONSTANT,
+        SEEK_LINEAR,
+        ARRIVE
+    };
+
+    [SerializeField]
+    private SteeringBehaviour state;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -23,20 +33,41 @@ public class Steering : MonoBehaviour
 
     void FixedUpdate()
     {
-        Seek();
+        float dt = Time.fixedDeltaTime;
+        Vector3 targetDirection = (target.position - transform.position).normalized;
+
+        switch(state)
+        {
+            case SteeringBehaviour.SEEK_CONSTANT:
+                SeekConstantSpeed(targetDirection);
+                break;
+
+            case SteeringBehaviour.SEEK_LINEAR:
+                SeekLinearSpeed(targetDirection);
+                break;
+        }
+
+        RotateTowards(targetDirection, dt);
     }
 
-    private void Seek()
+    private void SeekConstantSpeed(Vector3 targetDirection)
     {
-        float dt = Time.fixedDeltaTime;
-        Vector3 toTarget = (target.position - transform.position).normalized;
+        // Applies constant speed by subtracting direction vectors and multiplying the result by linear velocity.
+        Vector3 linearVelocityDirection = rb.velocity.normalized;
+        rb.AddForce((targetDirection - linearVelocityDirection) * linearSpeed);
+    }
 
-        // TODO -- make this accelerate?
-        Vector3 lvDir = rb.velocity.normalized;
-        rb.AddForce((toTarget - lvDir) * linearSpeed); 
+    private void SeekLinearSpeed(Vector3 targetDirection)
+    {
+        // Seek with increasing/decreasing speed by subtracting the constant of target direction *
+        // linear velocity by the ever-changing rigid body velocity to create a feedback loop!
+        rb.AddForce(targetDirection * linearSpeed - rb.velocity);
+    }
 
+    private void RotateTowards(Vector3 targetDirection, float dt)
+    {
         // Using AddTorque() to rotate towards a target is hard to control. This suffices.
-        Vector3 rotation = Vector3.RotateTowards(transform.forward, toTarget, dt * angularSpeed, 0.0f);
+        Vector3 rotation = Vector3.RotateTowards(transform.forward, targetDirection, dt * angularSpeed, 0.0f);
         transform.rotation = Quaternion.LookRotation(rotation);
     }
 }
